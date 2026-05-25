58388e95388119fe2bdab1edcf114fb1d7caaeed
ichwansh03 <ichwansholihin70@gmail.com>
2026-05-25 14:28:34 +0700

    rollback object mapper to generic jackson 2

--- src/main/kotlin/com/observability/sfdc/config/RedisConfig.kt
+++ src/main/kotlin/com/observability/sfdc/config/RedisConfig.kt
@@ -7,10 +7,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
-import tools.jackson.databind.ObjectMapper
 import java.time.Duration
 
 @Configuration
@@ -21,8 +20,8 @@ class RedisConfig(
 ) {
 
     @Bean
-    fun cacheManager(connectionFactory: RedisConnectionFactory, objectMapper: ObjectMapper): CacheManager {
-        val serializer = GenericJacksonJsonRedisSerializer(objectMapper)
+    fun cacheManager(connectionFactory: RedisConnectionFactory): CacheManager {
+        val serializer = GenericJackson2JsonRedisSerializer()
 
         val defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
             .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
