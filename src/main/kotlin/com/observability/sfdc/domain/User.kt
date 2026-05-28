195c9bcefdd2ec326e50ee5a54a056024d11d949
ichwansh03 <ichwansholihin70@gmail.com>
2026-05-28 13:18:46 +0700

    add json property for active user

--- src/main/kotlin/com/observability/sfdc/domain/User.kt
+++ src/main/kotlin/com/observability/sfdc/domain/User.kt
@@ -1,9 +1,12 @@
 package com.observability.sfdc.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.*
 
 @Entity
 @Table(name = "users")
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +19,9 @@ data class User(
     val username: String?,
     val email: String?,
     val profileName: String?,
+    
+    @JsonProperty("active")
     val isActive: Boolean?,
+    
     val entity: String?
 )
